b635c2977a21e8bda775533477e1a758a391d667
MarioEliasER <201G0247@rcarbonifera.tecnm.mx>
2023-10-10 15:13:00 -0600

    cambiosv3

---
+++ Models/ViewModels/IndexRazasViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Tarea2Perros.Models.ViewModels
+{
+    public class IndexRazasViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+    }
+}
