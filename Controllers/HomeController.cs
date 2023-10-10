082650ae25164e2fa6f4fb4120ed21afccc04173
MarioEliasER <201G0247@rcarbonifera.tecnm.mx>
2023-10-10 14:56:03 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tarea2Perros.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Info()
+		{
+			return View();
+		}
+	}
+}
---
+++ Models/Entities/Carreras.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tarea2Perros.Models.Entities;
+
+public partial class Carreras
+{
+    public int Id { get; set; }
+
+    public string Clave { get; set; } = null!;
+
+    public string Nombre { get; set; } = null!;
+
+    public string Plan { get; set; } = null!;
+
+    public string Especialidad { get; set; } = null!;
+
+    public string? Descripcion { get; set; }
+
+    public virtual ICollection<Materias> Materias { get; set; } = new List<Materias>();
+}
